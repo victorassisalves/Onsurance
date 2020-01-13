6f4698c0009069fbbf98b8fb81879d8e224f0437
Victor Assis <victor.assis@onsurance.me>
2020-01-13 18:16:48 -0300

    Adição do formulário de cotação de pneus no hosting. URL: api.onsurance.me

---
+++ public/precache-manifest.89c56bf6502aa24d9862e975bb920847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b36035b6a5b3c647947e4f17310bc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33527df9c9bd6bfa5d9f",
+    "url": "/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "a7dde6f63e5f8b027c9b",
+    "url": "/static/css/main.9ef4ae62.chunk.css"
+  },
+  {
+    "revision": "33527df9c9bd6bfa5d9f",
+    "url": "/static/js/2.52e353fb.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.52e353fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7dde6f63e5f8b027c9b",
+    "url": "/static/js/main.2287afa8.chunk.js"
+  },
+  {
+    "revision": "5dda549421750ad3923a",
+    "url": "/static/js/runtime-main.73bb812d.js"
+  }
+]);
